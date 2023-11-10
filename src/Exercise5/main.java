ae6a1d0c2999a9f899a0eb77a2cb4e4952e247cb
Alessio Guacci <alessioguacci@gmail.com>
2023-11-10 12:41:37 +0100

    Exercise 5  Ereditarietà 2:

--- src/Exercise5/main.java
+++ src/Exercise5/main.java
@@ -1,11 +1,15 @@
 package Exercise5;
 
+import java.awt.*;
+
 public class main {
     public static void main(String[] args) {
     Forma forma= new Forma();
     Triangolo triangolo= new Triangolo();
+    rectangle rectangle= new rectangle();
 
     forma.calcolaArea();
+    rectangle.calcolaArea();
     triangolo.calcolaArea();
     }
 }
---
+++ src/Exercise5/rectangle.java
@@ -0,0 +1,10 @@
+package Exercise5;
+
+public class rectangle extends Forma {
+    void calcolaArea(){
+        double length= 9;
+        double width= 4;
+        double area= length * width;
+        System.out.println("the area of the rectangle is "+ area);
+    }
+}
