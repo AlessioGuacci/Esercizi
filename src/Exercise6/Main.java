2911feb0858774d3858bbe6ffaf57b49a6e3452e
Alessio Guacci <alessioguacci@gmail.com>
2023-11-15 11:31:04 +0100

    Exercise 8: le interfacce (rifatto 5 e 6)

--- src/Exercise6/Main.java
+++ src/Exercise6/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main (String[] args) {
         Triangolo triangolo = new Triangolo();
 
-        System.out.println(triangolo.calcolaArea(7,8));
+        System.out.println(triangolo.calcolaArea(6,8));
 
 
 
--- src/Exercise6/Triangolo.java
+++ src/Exercise6/Triangolo.java
@@ -3,11 +3,9 @@ package Exercise6;
 import Esercizio5.Forma;
 
 public class Triangolo extends Forma {
-    private double height;
-    private double base;
+
 
     public double calcolaArea(double height, double base){
-        double area = ((height * base)/2);
-        return area;
+        return ((height * base)/2);
     }
 }
