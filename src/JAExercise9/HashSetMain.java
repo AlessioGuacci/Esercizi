d13b2b06a8a66006c353471582969b19c34ecf1c
Alessio Guacci <alessioguacci@gmail.com>
2023-12-01 17:07:36 +0100

    Exercise 9 JA: HashSet

--- src/JAExercise9/HashSetMain.java
+++ src/JAExercise9/HashSetMain.java
@@ -1,68 +1,35 @@
 package JAExercise9;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
-//cercato un po online ma ho mezzo impazzito per questo esercizio XD
+//ho fatto tutto solo prometto solo avevi ragione meglio vede obbietivo per obbietivo
 public class HashSetMain {
     public static void main(String[] args) {
-        HashSet <Integer> hashSet = new HashSet<>();
-        hashSet = hashSetFunction(hashSet);
-
-        MyObject myObject= new MyObject();
-        myObject.setA(1);
-        myObject.setB(2);
-        myObject.setC(3);
-
-        iteratorHashSets(hashSet, myObject);
-        System.out.println("hashset after removing elements would look like: ");
+        String a = "this is string";
+        String obj = new String(a);
+        HashSet hashSet= new HashSet<>();
+        fillUp(hashSet);
         System.out.println(hashSet);
+        System.out.println(comparison(hashSet,obj));
+
+
     }
-
-    public static HashSet <Integer> hashSetFunction (HashSet <Integer> input){
-        Set <Integer> setInput = new HashSet<>();
-        setInput.add(1);
-        setInput.add(2);
-        setInput.add(3);
-        input.addAll(setInput);
-        return input;
+    public static HashSet <String> fillUp (HashSet<String> hashSetInput){
+        Set <String> setInput = new HashSet<>();
+        setInput.add("this is string");
+        hashSetInput.addAll(setInput);
+        return hashSetInput;
     }
-
-public static void iteratorHashSets(HashSet <Integer> hashSetInput, MyObject obj) {
-    hashSetInput.removeIf(element -> element.equals(obj.getA()));
-    hashSetInput.removeIf(element-> element.equals(obj.getB()));
-    hashSetInput.removeIf(element-> element.equals(obj.getC()));
-}
-
-    static class MyObject {
-
-        private Integer a;
-        private Integer b;
-        private Integer c;
-
-        public void setA(Integer a) {
-            this.a = a;
-        }
-
-        public void setB(Integer b) {
-            this.b = b;
-        }
-
-        public void setC(Integer c) {
-            this.c = c;
-        }
-
-        public Integer getA() {
-
-            return a;
-        }
-
-        public Integer getB() {
-            return b;
-        }
-
-        public Integer getC() {
-            return c;
-        }
+    public static HashSet<String> comparison (HashSet <String> hashSetInput, String objInput){
+       for (String i : hashSetInput){
+           if (Objects.equals(i, objInput)){
+               hashSetInput.remove(objInput);
+           }
+       }
+       return hashSetInput;
     }
 }
+
+
