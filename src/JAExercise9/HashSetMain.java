28168b0a9a99417c700370bb9c6d36cf67b15c24
Alessio Guacci <alessioguacci@gmail.com>
2023-12-03 11:55:33 +0100

    Exercise 9 JA: HashSet (rifatto tutto)

--- src/JAExercise9/HashSetMain.java
+++ src/JAExercise9/HashSetMain.java
@@ -2,40 +2,28 @@ package JAExercise9;
 
 import java.util.HashSet;
 import java.util.Iterator;
-import java.util.Set;
 
-//fatto sempre solo ma non trovavo la syntax giusto quindi ho messo un po
+//ho rifatto tutto da capo, stavo interpretando male le domande e mi sono reso conto che era molto piu semplice del dovuto
+//spero vada bene
 public class HashSetMain {
     public static void main(String[] args) {
-        String a = "this is string";
-        String obj = a;
-        HashSet hashSet= new HashSet<>();
-        hashSet.add("this is new string");
-        fillUp(hashSet);
-        System.out.println(hashSet);
-        System.out.println(comparison(hashSet,obj));
-
-
+        Hashset();
     }
-    public static HashSet <String> fillUp (HashSet<String> hashSetInput){
-        Set <String> setInput = new HashSet<>();
-        setInput.add("this is string");
-        hashSetInput.addAll(setInput);
-        return hashSetInput;
-    }
-    public static HashSet<String> comparison (HashSet <String> hashSetInput, String objInput){
-        Iterator <String> it = hashSetInput.iterator();
+    public static void Hashset (){
+        HashSet <String> newList = new HashSet<>();
+        newList.add("iphone");
+        newList.add("samsung");
+        newList.add("nokia");
+        String object = "nokia";
+        Iterator <String> it = newList.iterator();
         while (it.hasNext()){
-            String contents = it.next();
-            if (contents.equals(objInput)){
-                it.remove();
+            if (newList.contains(object)){
+                newList.remove(object);
                 break;
             }
-
-           }
-        return hashSetInput;
+        }
+        System.out.println(newList);
     }
-
 }
 
 
