7bcc16c755ffe9bf09e59017fe21bc555a9198ad
Alessio Guacci <alessioguacci@gmail.com>
2023-12-05 17:48:50 +0100

    Exercise 15 JA: Record

---
+++ src/JAExercise15/Point.java
@@ -0,0 +1,4 @@
+package JAExercise15;
+
+public record Point(int x, int y) {
+}
---
+++ src/JAExercise15/RecordMain.java
@@ -0,0 +1,9 @@
+package JAExercise15;
+
+//forse ho capito male la domanda o e semplicemente questo?
+public class RecordMain {
+    public static void main(String[] args) {
+        Point point = new Point(5,6);
+        System.out.println(point);
+    }
+}
