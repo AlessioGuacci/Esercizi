c5d9b79cbabfbdb89e8441868d01258ac67c437f
Alessio Guacci <alessioguacci@gmail.com>
2023-12-06 16:22:48 +0100

    Exercise 19 JA: Date

--- src/JAExercise19/Date3.java
+++ src/JAExercise19/Date3.java
@@ -1,6 +1,9 @@
 package JAExercise19;
 
 import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class Date3 {
     public static void main(String[] args) {
@@ -9,8 +12,12 @@ public class Date3 {
         OffsetDateTime newYear = modifiableDate.plusYears(1);
         OffsetDateTime subtractMonth = modifiableDate.minusMonths(1);
         OffsetDateTime addDays = modifiableDate.plusWeeks(1);
-        System.out.println(newYear);
-        System.out.println(subtractMonth);
-        System.out.println(addDays);
+
+        DateTimeFormatter formatter= DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+                .withLocale(Locale.ITALIAN).withZone(ZoneId.of("Europe/Rome"));
+
+        System.out.println(newYear.format(formatter));
+        System.out.println(subtractMonth.format(formatter));
+        System.out.println(addDays.format(formatter));
     }
 }
