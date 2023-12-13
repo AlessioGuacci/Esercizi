4ac4fb8532cc759e892da24f8ebabb78d9082a63
Alessio Guacci <alessioguacci@gmail.com>
2023-12-13 19:04:30 +0100

    Exercise 23 JA: Test 3

--- src/JAExercise24/DateSpecifics.java
+++ src/JAExercise24/DateSpecifics.java
@@ -8,6 +8,7 @@ public class DateSpecifics {
 
     public static void main(String[] args) {
         OffsetDateTime date = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+        dateFormater1(date);
 
 
     }
