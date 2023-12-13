ab6185a81c5c04be1152170e2c794b4608199097
Alessio Guacci <alessioguacci@gmail.com>
2023-12-13 17:21:49 +0100

    Exercise 23 JA: Test 3

---
+++ src/JAExercise23/Date2023.java
@@ -0,0 +1,13 @@
+package JAExercise23;
+
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
+public class Date2023 {
+    public static void main(String[] args) {
+        OffsetDateTime date = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+        String datePrint = date.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG));
+        System.out.println(datePrint);
+    }
+}
---
+++ src/JAExercise23/Date2023Test.java
@@ -0,0 +1,19 @@
+package JAExercise23;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Date2023Test {
+
+    @Test
+    void main() {
+        OffsetDateTime date = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+        String dateTest= date.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG));
+        assertEquals("1 marzo 2023", dateTest);
+    }
+}
