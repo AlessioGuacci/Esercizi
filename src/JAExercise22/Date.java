d6d2c1f036640d724757eb05e441d416b45ad3ad
Alessio Guacci <alessioguacci@gmail.com>
2023-12-14 16:02:05 +0100

    Exercise 22 JA: Test 2

--- src/JAExercise22/Date.java
+++ src/JAExercise22/Date.java
@@ -4,16 +4,16 @@ import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 
-//se non e giusto mi puoi mandare un esempio su discord di come risolvere lesercizio cosi capisco dove sbaglio,
-//non capisco per niente come va spiegato nel video scusami Nicola
 
 public class Date {
     public static void main(String[] args) {
+
+        Date myDate= new Date();
         OffsetDateTime date = OffsetDateTime.parse("2002-03-01T13:00:00Z");
-        dateFormater(date);
+        myDate.dateFormater(date);
 
     }
-        public static String[] dateFormater(OffsetDateTime date) {
+        public  String[] dateFormater(OffsetDateTime date) {
             String dateFull = date.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG));
             String dateMedium = date.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM));
             String dateShort = date.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT));
--- src/JAExercise22/DateTest.java
+++ src/JAExercise22/DateTest.java
@@ -8,10 +8,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class DateTest {
 
+
+
     @Test
     void dateTester() {
-        OffsetDateTime date = OffsetDateTime.parse("2002-03-01T13:00:00Z");
-        String [] formattedDates= Date.dateFormater(date);
+        Date date= new Date();
+        OffsetDateTime date1 = OffsetDateTime.parse("2002-03-01T13:00:00Z");
+        String [] formattedDates= date.dateFormater(date1);
         assertEquals("1 marzo 2002",formattedDates[0]);
         assertEquals("1 mar 2002", formattedDates[1]);
         assertEquals("01/03/02",formattedDates[2]);
