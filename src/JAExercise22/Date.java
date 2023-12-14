3ab34f39f0a000cd528c0028c4acaf82cf3410cc
Alessio Guacci <alessioguacci@gmail.com>
2023-12-14 11:31:56 +0100

    Exercise 22 JA: Test 2

--- src/JAExercise22/Date.java
+++ src/JAExercise22/Date.java
@@ -4,13 +4,16 @@ import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 
+//se non e giusto mi puoi mandare un esempio su discord di come risolvere lesercizio cosi capisco dove sbaglio,
+//non capisco per niente come va spiegato nel video scusami Nicola
+
 public class Date {
     public static void main(String[] args) {
         OffsetDateTime date = OffsetDateTime.parse("2002-03-01T13:00:00Z");
         dateFormater(date);
 
     }
-        public static void dateFormater(OffsetDateTime date) {
+        public static String[] dateFormater(OffsetDateTime date) {
             String dateFull = date.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG));
             String dateMedium = date.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM));
             String dateShort = date.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT));
@@ -18,5 +21,6 @@ public class Date {
             System.out.println(dateFull);
             System.out.println(dateMedium);
             System.out.println(dateShort);
+            return new String[] {dateFull,dateMedium,dateShort};
         }
 }
--- src/JAExercise22/DateTest.java
+++ src/JAExercise22/DateTest.java
@@ -3,6 +3,7 @@ package JAExercise22;
 import org.junit.jupiter.api.Test;
 
 import java.time.OffsetDateTime;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 class DateTest {
@@ -10,6 +11,9 @@ class DateTest {
     @Test
     void dateTester() {
         OffsetDateTime date = OffsetDateTime.parse("2002-03-01T13:00:00Z");
-        Date.dateFormater(date);
+        String [] formattedDates= Date.dateFormater(date);
+        assertEquals("1 marzo 2002",formattedDates[0]);
+        assertEquals("1 mar 2002", formattedDates[1]);
+        assertEquals("01/03/02",formattedDates[2]);
     }
 }
