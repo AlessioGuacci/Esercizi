bc2a416a152f91818f79b6d35960b07347bc49c9
Alessio Guacci <alessioguacci@gmail.com>
2023-12-14 13:23:38 +0100

    Exercise 25 JA: Test 5

---
+++ src/JAExercise25/DateAdderTest.java
@@ -0,0 +1,22 @@
+package JAExercise25;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.OffsetDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+//2024-03-01T13:00:00Z
+//        2023-02-01T13:00:00Z
+//        2023-03-08T13:00:00Z
+class DateAdderTest {
+
+    @Test
+    void dateformator() {
+        OffsetDateTime date = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+        String [] formattedDate= DateAdder.dateformator(date);
+        assertEquals("2024-03-01T13:00:00Z",formattedDate[0]);
+        assertEquals("2023-02-01T13:00:00Z",formattedDate[1]);
+        assertEquals("2023-03-08T13:00:00Z",formattedDate[2]);
+    }
+}
